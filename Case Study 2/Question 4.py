6c2d09746871d418c99027604aebee5f8b1bcd6f
Abinash Panda <iamabinashpanda@outlook.com>
2026-01-07 14:58:54 +0530

    Update Question 4.py in Case Study 2

---
+++ Case Study 2/Question 4.py
@@ -0,0 +1,7 @@
+# Please  write  a  program that accepts  a  string  from the console  and  prints it  in reverse order.
+# Example
+# If the following string is given as input to the program: welcome to edureka
+# Then, the output of the program should be:akerude ot emoclew
+
+sentence = input("Enter a sentence :")
+print(sentence[::-1])
